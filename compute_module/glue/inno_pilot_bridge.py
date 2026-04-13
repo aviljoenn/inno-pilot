3a820a373c92d09c165d66b26d7ffbf60784bd69
andreviljoenn <you@yourdomain.com>
2026-04-13 11:47:36 +0200

    Fix for the Build number alignment.

--- compute_module/glue/inno_pilot_bridge.py
+++ compute_module/glue/inno_pilot_bridge.py
@@ -73,7 +73,7 @@ if hasattr(signal, "SIGUSR1"):
 # Inno-Pilot version (must match Nano firmware + remote firmware )
 # ---------------------------------------------------------------------------
 INNOPILOT_VERSION   = "v1.2.0_B31"
-INNOPILOT_BUILD_NUM = 29  # increment with each push during development
+INNOPILOT_BUILD_NUM = 31  # increment with each push during development
 
 # ---------------------------------------------------------------------------
 # Serial devices
