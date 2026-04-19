e0288d05d29376d733a79e0a8d963e6080f386fc
andreviljoenn <you@yourdomain.com>
2026-04-19 07:18:24 +0200

    Web Remote Helm wheel direction inverted. Try 2 version _B41

--- compute_module/glue/inno_pilot_bridge.py
+++ compute_module/glue/inno_pilot_bridge.py
@@ -87,8 +87,8 @@ OTA_SERVER_HOST = _local_ip()
 # ---------------------------------------------------------------------------
 # Inno-Pilot version (must match Nano firmware + remote firmware )
 # ---------------------------------------------------------------------------
-INNOPILOT_VERSION   = "v1.2.0_B39"
-INNOPILOT_BUILD_NUM = 39  # increment with each push during development
+INNOPILOT_VERSION   = "v1.2.0_B41"
+INNOPILOT_BUILD_NUM = 41  # increment with each push during development
 
 # ---------------------------------------------------------------------------
 # Serial devices
